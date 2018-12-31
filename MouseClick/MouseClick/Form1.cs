2df85f84e6846333373ee65141ed8eb4e8e9e7c5
rabbit860321 <rabbit860321@gmail.com>
2018-12-31 10:39:15 +0800

    add ex

---
+++ MouseClick/MouseClick/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseClick
+{
+    public partial class Form1 : Form
+    {
+        int times = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            times++;
+
+            timeslabel.Text = "你已經按了" + times + "次";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            times = 0;
+
+            timeslabel.Text = "你已經按了" + times + "次";
+        }
+    }
+}
